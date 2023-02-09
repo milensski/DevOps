401880e497e7433015ae9bc1d5bf192d2f8cd57a
Milen <feniks9511@gmail.com>
2023-02-09 11:22:21 +0200

    added lines

--- docker-apache/docker-setup.sh
+++ docker-apache/docker-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "* Add hosts ..."
-echo "192.168.199.100 docker.do1.lab docker" >> /etc/hosts
+echo "192.168.199.100 apache-web docker" >> /etc/hosts
+
 
 echo "* Add Docker repository ..."
 dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
@@ -19,3 +20,21 @@ firewall-cmd --reload
 
 echo "* Add vagrant user to docker group ..."
 usermod -aG docker vagrant
+
+docker build -t apache-centos - << EOF
+FROM    centos:7
+RUN yum install httpd httpd-tools -y
+
+EXPOSE  80
+
+CMD     ["/usr/sbin/httpd","-D","FOREGROUND"]
+
+EOF
+
+docker run -d -p 8090:80 -v /home/user/web:/var/www/html apache-centos
+
+echo "<h1>This is Milens container</h1>" >> /home/user/web/index.html
+
+cd /home/user/web/ || return
+
+sudo docker cp index.html apache-centos:/var/www/apache-web/html
