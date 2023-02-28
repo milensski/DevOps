74b8fb0300bd2e5cc8da0567c92a9deaaa2d399e
Milen <feniks9511@gmail.com>
2023-02-28 20:24:45 +0200

    refactored

--- vagrant-jenkins-docker/jenkins-setup.sh
+++ vagrant-jenkins-docker/jenkins-setup.sh
@@ -18,16 +18,41 @@ sudo apt-get -y install git
 echo "* Install Jenkins ..."
 sudo apt-get update
 sudo apt-get -y install jenkins
-# sudo service jenkins start
 
-# sleep 1m
-# sudo usermod -s /bin/bash jenkins
-# sudo passwd jenkins
-# su - jenkins
-# pwd
-# ssh-keygen -t ecdsa -b 521 -m PEM
+sudo systemctl enable jenkins
 
-# echo "Installing Jenkins Plugins"
-# JENKINSPWD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
-# echo $JENKINSPWD
 
+
+# Get the initial admin password
+PASSWORD=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
+
+# Unlock Jenkins with the initial admin password
+sudo echo "jenkins.model.Jenkins.instance.securityRealm.createAccount(\"admin\", \"$PASSWORD\")" | sudo java -jar /usr/share/jenkins/jenkins.war --webroot=/var/cache/jenkins/war --httpPort=8080 --ajp13Port=-1 --httpsPort=-1 --sessionTimeout=86400 --accessLoggerClassName=winstone.accesslog.SimpleAccessLogger --simpleAuth.hudson.model.SimpleUserDetailsService.user=admin
+
+# Restart Jenkins
+sudo systemctl restart jenkins
+
+sudo usermod -s /bin/bash jenkins
+# Define the new password for the jenkins user
+NEW_PASSWORD="Password1"
+
+# Use the passwd command to encrypt the new password
+ENCRYPTED_PASSWORD=$(echo "$NEW_PASSWORD" | passwd --stdin jenkins)
+
+# Use the chpasswd command to update the password in the system password database
+echo "jenkins:$ENCRYPTED_PASSWORD" | chpasswd
+
+
+#Add jenkins user to sudoers list
+sudo bash -c "echo 'jenkins ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
+
+sudo mkdir -p /projects/www-static
+sudo chown -R jenkins:jenkins /projects
+
+#Generate an SSH key, accept all default values
+ssh-keygen -t ecdsa -b 521 -m PEM
+
+#Copy the key to the localhost
+sshpass -p Password1 ssh-copy-id jenkins@localhost
+
+sudo systemctl restart jenkins
